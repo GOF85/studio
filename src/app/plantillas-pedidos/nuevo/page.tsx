df52df6b7b5564fd16951cfc7d530494aef6ee9a
Guillermo Otero <guillermo.otero@micecatering.com>
2025-09-22 10:11:25 +0000

    no funciona (_for element <LinkComponent>_)

--- src/app/plantillas-pedidos/nuevo/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import PlantillaFormPage from '../[id]/page';
-
-// This component is intentionally left simple.
-// The logic is handled in the dynamic route page `../[id]/page.tsx`.
-export default function NuevaPlantillaPage() {
-    return <PlantillaFormPage />;
-}
