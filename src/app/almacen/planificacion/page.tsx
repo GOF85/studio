1aa87541091fc1ce33e1b6078c5331ecb239f99b
Guillermo Otero <guillermo.otero@micecatering.com>
2025-10-06 15:31:23 +0000

    Try fixing this error: `Runtime Error: Error: Link is not defined. Error

--- src/app/almacen/planificacion/page.tsx
+++ src/app/almacen/planificacion/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import Link from 'next/link';
 import { useState, useEffect, useMemo, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import { DateRange } from 'react-day-picker';
