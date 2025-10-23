e545f7394f64d30624088869d65b2558a86b29aa
Guillermo Otero <guillermo.otero@micecatering.com>
2025-10-23 15:36:10 +0000

    en /book/ingredientes al tratar de crear nuevo ingrediente da error la p

--- src/app/book/ingredientes/nuevo/page.tsx
+++ src/app/book/ingredientes/nuevo/page.tsx
@@ -1,16 +1,16 @@
 
-
 'use client';
 
 import { useRouter } from 'next/navigation';
-import IngredienteFormPage from '../[id]/page';
+import { useEffect } from 'react';
+
 
 export default function NuevoIngredientePage() {
     const router = useRouter();
 
-    // Reutilizamos el componente del formulario de edición, 
-    // ya que la lógica es casi idéntica y se maneja por la ausencia de un `id` en la URL.
-    // La página [id] ya contempla el caso de "nuevo".
-    
-    return <IngredienteFormPage />;
+    useEffect(() => {
+        router.replace('/book/ingredientes/nuevo');
+    }, [router]);
+
+    return null;
 }
