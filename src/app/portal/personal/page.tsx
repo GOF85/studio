9334ceb4929ab797968e71e290570aa2f00a30d1
Guillermo Otero <guillermo.otero@micecatering.com>
2025-11-03 16:23:14 +0000

    Try fixing this error: `Runtime Error: Error: "" is not a function. Erro

--- src/app/portal/personal/page.tsx
+++ src/app/portal/personal/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import { Briefcase, Building2, Calendar as CalendarIcon, CheckCircle, Clock, Factory, User, Users, ArrowLeft, ChevronLeft, ChevronRight, Edit, MessageSquare, Pencil, PlusCircle, RefreshCw, Send, Trash2, AlertTriangle, Printer, FileText, Upload, Phone, Save, Loader2 } from 'lucide-react';
 import { format, isSameMonth, isSameDay, add, sub, startOfWeek, endOfWeek, startOfMonth, endOfMonth, eachDayOfInterval, isBefore, startOfToday, isWithinInterval, endOfDay } from 'date-fns';
 import { es } from 'date-fns/locale';
-import { useForm } from 'react-hook-form';
+import { useForm, FormProvider } from 'react-hook-form';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
@@ -633,5 +633,4 @@ export default function PortalPersonalPage() {
         </TooltipProvider>
     );
 }
-```
 ```
